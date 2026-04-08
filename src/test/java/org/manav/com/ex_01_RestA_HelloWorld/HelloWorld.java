ca2d1fa8b1d4209f6e2d9ddee864a5294c245e4e
manav <manavmalhotra2020@gmail.com>
2026-04-09 00:46:23 +0530

    Setup API Project

--- src/test/java/org/manav/com/ex_01_RestA_HelloWorld/HelloWorld.java
+++ src/test/java/org/manav/com/ex_01_RestA_HelloWorld/HelloWorld.java
@@ -1,4 +1,31 @@
 package org.manav.com.ex_01_RestA_HelloWorld;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
 public class HelloWorld {
+
+     @Test
+    public void testHelloWorld(){
+//         System.out.println("Hello World");
+
+         // Rest Assured
+
+         // Given - pre-requisite
+
+         // When - Action
+
+         // Then - Validation
+
+         RestAssured.
+                 given()
+                    .baseUri("https://restful-booker.herokuapp.com/")
+                    .basePath("/ping")
+                 .when()
+                    .get()
+                 .then()
+                    .statusCode(201);
+
+     }
+
 }
